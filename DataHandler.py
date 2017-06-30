5b888134dfe7bdf084d5351085cc3a0e38560e00
Lyserg25 <s0539720@htw-berlin.de>
2017-06-30 14:29:24 +0200

    adapted script for data handler

--- DataHandler.py
+++ DataHandler.py
@@ -4,7 +4,7 @@ import requests
 from KeywordExtraction import KeywordExtraction
 
 
-def handle_reports(db):
+def handle_reports(db, reports):
     dbc = db.cursor()
     # dbc.execute("DROP TABLE IF EXISTS reports")
     # sql = """CREATE TABLE reports (
@@ -14,7 +14,7 @@ def handle_reports(db):
     #     PRIMARY KEY (id) )"""
     # dbc.execute(sql)
 
-    reports = requests.get('http://cmts3.f4.htw-berlin.de:8000/reports?page=1').json()
+
     keywordextaction = KeywordExtraction()
     i = -1
 
@@ -22,8 +22,8 @@ def handle_reports(db):
     # keywords = keywordextaction.extract_keywords(bug['inhalt'], bug['fachbegriffe'])
 
     for report in reports:
-        # for i in range(33, 1000):
-        # report = reports[i]
+    # for i in range(614, 1000):
+    #     report = reports[i]
         keywords = ''
         summary = ''
         i += 1
@@ -31,18 +31,21 @@ def handle_reports(db):
         try:
             r = requests.get('http://extractor.f4.htw-berlin.de/report_id/' + report['id']).json()
             terms = r['fachbegriffe']
-        except:
+        except Exception as e:
+            print(e)
             terms = {}
             print('Calculation of terms failed for report ' + report['id'])
 
         try:
             keywords = keywordextaction.extract_keywords(report['inhalt'], terms)
-        except:
+        except Exception as e:
+            print(e)
             print('Keyword extraction failed for report ' + str(i) + 'id: ' + report['id'])
 
         try:
             summary = keywordextaction.extract_sentences(report['inhalt'], terms)
-        except:
+        except Exception as e:
+            print(e)
             print('Sentence extraction failed for report ' + str(i) + 'id: ' + report['id'])
 
         sql = """INSERT INTO reports VALUES (%s, %s, %s)"""
@@ -51,12 +54,13 @@ def handle_reports(db):
             dbc.execute(sql, (report['id'], keywords, summary))
             db.commit()
             # print('Inserted report ' + report['id'])
-        except:
+        except Exception as e:
+            print(e)
             db.rollback()
             print('Insert failed for report ' + report['id'])
 
 
-def handle_articles(db):
+def handle_articles(db, articles):
     dbc = db.cursor()
 
     # dbc.execute("DROP TABLE IF EXISTS articles")
@@ -67,24 +71,26 @@ def handle_articles(db):
     #     PRIMARY KEY (id) )"""
     # dbc.execute(sql)
 
-    articles = requests.get('http://cmts3.f4.htw-berlin.de:8000/articles?page=1').json()
+    # articles = requests.get('http://cmts3.f4.htw-berlin.de:8000/articles?page=3').json()
     keywordextaction = KeywordExtraction()
     i = -1
 
     for article in articles:
-    #for i in range(14, 1000):
-     #   article = articles[i]
+    #for i in range(23, 1000):
+        # article = articles[i]
         keywords = ''
         summary = ''
         i += 1
         try:
             keywords = keywordextaction.extract_keywords(article['inhalt'])
-        except:
+        except Exception as e:
+            print(e)
             print('Keyword extraction failed for article ' + str(i) + 'id: ' + article['id'])
 
         try:
             summary = keywordextaction.extract_sentences(article['inhalt'])
-        except:
+        except Exception as e:
+            print(e)
             print('Sentence extraction failed for article ' + str(i) + ' id: ' + article['id'])
 
         sql = """INSERT INTO articles VALUES (%s, %s, %s)"""
@@ -92,12 +98,17 @@ def handle_articles(db):
             dbc.execute(sql, (article['id'], keywords, summary))
             db.commit()
             # print('Inserted article ' + article['id'])
-        except:
+        except Exception as e:
+            print(e)
             db.rollback()
             print('Insert failed for article ' + article['id'])
 
 
 db = pymysql.connect('db.f4.htw-berlin.de', 's0539720', 'Start123#', '_s0539720__reports', charset='utf8')
-# handle_articles(db)
-handle_reports(db)
+
+for i in range(4, 24):
+    articles = requests.get('http://cmts3.f4.htw-berlin.de:8000/articles?page=' + str(i)).json()
+    handle_articles(db, articles)
+    print("seite " + i + " fertig")
+
 db.close()
